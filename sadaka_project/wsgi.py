50a8ffb0ecd3b35722da54c0a2a82b6b9f88b800
KWASIC <your.email@example.com>
2024-12-04 02:53:53 +0000

    Fix WSGI configuration and environment variables

--- sadaka_project/wsgi.py
+++ sadaka_project/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sadaka_project.settings')
+# Add project directory to Python path
+path = '/home/Calvin31/CAL-CARES'
+if path not in sys.path:
+    sys.path.append(path)
 
+# Set environment variables
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sadaka_project.settings'
+os.environ['PAYSTACK_PUBLIC_KEY'] = 'pk_live_55ef871debbd9948a6fb282b39b2dd5404fabf56'
+os.environ['PAYSTACK_SECRET_KEY'] = 'sk_live_4d2f5e2f7f9e070df5c6aa9e0c4e5e1f0c0d0b0a'
+
+# Initialize WSGI application
 application = get_wsgi_application()
